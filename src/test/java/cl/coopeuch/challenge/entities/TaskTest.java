132e24528d162c9780164e9574f26cc08bc7449a
Jonathan Contreras <jcontreras@oozma.com>
2021-10-13 21:30:22 -0400

    Agrega pruebas a Task

--- src/test/java/cl/coopeuch/challenge/entities/TaskTest.java
+++ src/test/java/cl/coopeuch/challenge/entities/TaskTest.java
@@ -1,4 +1,20 @@
 package cl.coopeuch.challenge.entities;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TaskTest {
+    @Test
+    public void testGetAndSetFieldsInTask() {
+        LocalDateTime localDateTime = LocalDateTime.of(2021, 10, 13, 10, 10);
+        Task task = TaskMock.getWithId();
+        assertEquals(10, task.getId());
+        assertEquals("blablabla", task.getDescription());
+        assertEquals(localDateTime, task.getCreationDate());
+        assertTrue(task.isActive());
+    }
 }
