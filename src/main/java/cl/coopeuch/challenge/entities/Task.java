18e2961031ec6c14838c1b92ecd6de17f2633d64
Jonathan Contreras <jcontreras@oozma.com>
2021-10-17 09:20:38 -0400

    Formatear fecha para ser leida desde React

--- src/main/java/cl/coopeuch/challenge/entities/Task.java
+++ src/main/java/cl/coopeuch/challenge/entities/Task.java
@@ -1,5 +1,6 @@
 package cl.coopeuch.challenge.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.persistence.*;
@@ -23,6 +24,7 @@ public class Task {
     private String description;
     @Column(name = "FECHA_CREACION")
     @NotNull(message = "Debe indicar la fecha de creacion")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'hh:mm:ss.SSSSSS'Z'")
     private LocalDateTime creationDate;
     @Column(name = "ES_VIGENTE", columnDefinition = "BIT")
     @NotNull(message = "Debe indicar si es o no vigente")
